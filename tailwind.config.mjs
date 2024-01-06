22bbb0f84c9c15bc61b7bad05ed273a7150796b8
ClaudCZX <claudiodelafuented@gmail.com>
2024-01-05 21:24:15 -0300

    feature: add structure for portfolio and add components AsideMenu

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'color1': '#F20587',
+			'color2': '#F205CB',
+			'color3': '#63038C',
+			'color4': '#2D0140',
+			'color5': '#010326'
+    }
+	},
+	plugins: [],
+}
